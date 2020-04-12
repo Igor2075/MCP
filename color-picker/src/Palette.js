5e4fbe013d69e5c21e1b6384906b2afe15ad241a
Igor <rogier.van.amersfoort.75@gmail.com>
2020-04-12 13:58:57 -0300

    Slider Navbar

--- color-picker/src/Palette.js
+++ color-picker/src/Palette.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
 import "./Palette.css";
+
 export default class Palette extends Component {
+	state = {
+		level: 500,
+	};
+	changeLevel = (level) => {
+		this.setState({ level });
+	};
 	render() {
-		const colorBoxes = this.props.colors.map((color) => (
-			<ColorBox background={color.color} name={color.name} />
+		const { level } = this.state;
+		const { colors } = this.props.palette;
+		const colorBoxes = colors[level].map((color) => (
+			<ColorBox background={color.hex} name={color.name} />
 		));
 		return (
 			<div className="Palette">
+				<Navbar level={level} changeLevel={this.changeLevel} />
 				<div className="Palette-colors">{colorBoxes}</div>
 			</div>
 		);
