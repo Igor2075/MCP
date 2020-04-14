adb02240fefd2f8d10d96df705f2140e5aad9c9c
Igor <rogier.van.amersfoort.75@gmail.com>
2020-04-13 23:28:04 -0300

    Simgle Color Palette added

--- color-picker/src/Palette.js
+++ color-picker/src/Palette.js
@@ -16,9 +16,15 @@ export default class Palette extends Component {
 	};
 	render() {
 		const { level, format } = this.state;
-		const { colors, paletteName, emoji } = this.props.palette;
+		const { colors, paletteName, emoji, id } = this.props.palette;
 		const colorBoxes = colors[level].map((color) => (
-			<ColorBox background={color[format]} name={color.name} key={color.name} />
+			<ColorBox
+				background={color[format]}
+				name={color.name}
+				key={color.name}
+				moreURL={`/palette/${id}/${color.id}`}
+				showLink={true}
+			/>
 		));
 		return (
 			<div className="Palette">
